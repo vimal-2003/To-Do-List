6a40ca47cf51385fd4fdbd5b9c9742f7a395ec4b
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-05-01 19:34:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function addTask() {
     if (inputBox.value === '') {
         alert("You must write something!");
     } 
-    else {
+    else
+    {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.append(li);
