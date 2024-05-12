8ca3cee8e2619a3cb1aab4272592e020a332a49b
VIMAL N <146964858+vimal-2003@users.noreply.github.com>
2024-05-12 15:24:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function addTask()
     saveData();
     
 }
+
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
